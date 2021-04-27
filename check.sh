7e5cf7e3cbb3be810fe43b19eb4b065579d17152
lulzette <lulzette@cock.li>
2021-04-27 20:25:34 +0300

    fix check

--- check.sh
+++ check.sh
@@ -15,10 +15,10 @@ source $full_path/config_list.sh
 #if pid exists and stream is live, than exit and do not start recording
 [ -f $storage_path/$1/pid ] && $full_path/lifeChk.py $1 $twitchid && exit 0
 
-echo $$ > $storage_path/$1/pid
-
 #exit if no stream and remove lock
-$full_path/lifeChk.py $1 $twitchid || rm $storage_path/$1/pid || exit 0
+$full_path/lifeChk.py $1 $twitchid || rm $storage_path/$1/pid ; exit 0
+
+echo $$ > $storage_path/$1/pid
 
 #set pid and start recording
 /home/losted/.local/bin/youtube-dl -v -o $storage_path/$1/"%(upload_date)s_%(title)s__%(timestamp)s_%(id)s.%(ext)s" twitch.tv/$1 >> $storage_path/$1/youtube-dl.log 2>&1 
