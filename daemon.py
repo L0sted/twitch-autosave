6a8f33d5b8252e7251b2aaa3d6ab1b98ac6e9db2
lulzette <ivan@lulzette.ru>
2023-06-02 00:11:00 +0300

    Мини фикс формата сообщения

--- daemon.py
+++ daemon.py
@@ -13,7 +13,7 @@ import time
 import logging
 from logging.handlers import TimedRotatingFileHandler
 
-log_format = logging.Formatter('%(asctime)s %(levelname)s:%(message)s')
+log_format = logging.Formatter('%(asctime)s %(levelname)s - %(message)s')
 log_file = 'output.log'
 cfg_file = 'config.ini'
 
